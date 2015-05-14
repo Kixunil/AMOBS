7d14586d9361d07913709691e14291bd4088d128
Martin Habovštiak <martin.habovstiak@gmail.com>
2015-05-14 17:04:36 +0200

    Added comments and fixed crashing bug

--- parser.py
+++ parser.py
@@ -2,14 +2,17 @@
 
 import re
 
+# Regex for finding the beginning of log
 dateRegex = re.compile(r"^[1-9][0-9]{3,} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) +[1-9][0-9]? +[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9]{3}")
 
+# Finds next beginning of the log, skipping everything else
 def findDate(f):
     for line in f:
         if dateRegex.match(line):
             return True
     return False
 
+# Parses key-value pairs (those before tables), supports : and = separators
 def parseKV(l):
     separator = l.find(':')
     if separator < 0:
@@ -20,9 +23,11 @@ def parseKV(l):
 
     return key.strip(), val.strip()
 
+# Recursive function which creates list of columns
 def slice_columns(header, b, e, prefix):
     last = b
     i = b
+    # This loop searches for pipes inside column name and constructs new columns and their names if they are found
     while i < e:
         if header[0][i] == '|':
             if len(header) > 1:
@@ -42,12 +47,15 @@ def slice_columns(header, b, e, prefix):
         else:
             yield prefix + header[0][last:i].strip()
 
-
+# This function parses any table found inside log
 def parseTable(f):
     line = f.next().strip()
     header = []
 
     while len(line) > 2 and line[1] != '-':
+        if line[0] != '|':
+            print "Error: not a table"
+            return
         header.append(line)
         line = f.next().strip()
 
@@ -63,20 +71,34 @@ def parseTable(f):
     while len(line) > 0:
         line = f.next().strip()
 
+# Program beginning
+# Open log
 f = open("final set/A.txt")
+
+# Find first log line, skip anything else at the beginning
 findDate(f)
 
+# Prepare key-value regex
 kvRegex = re.compile(".*[:=].*")
 
 try:
     while True:
+        # Iterate over lines
         line = f.next().strip()
+
+        # Skip empty lines
         if line == "":
             pass
+
+        # Log beginning found
+        # TODO: store new log information
         elif dateRegex.match(line):
             pass
+        # Check if line is beginning of a table
         elif line[0] == '-' or line[0] == '|':
             parseTable(f)
+
+        # Check if line is key-value
         elif kvRegex.match(line):
             key, value = parseKV(line)
             print key, value
