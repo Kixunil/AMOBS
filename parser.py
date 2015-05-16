1ae6b0d2952712f736d3917b77c7288986db9335
Martin Habovštiak <martin.habovstiak@gmail.com>
2015-05-16 11:54:48 +0200

    Implemented storing into database

--- parser.py
+++ parser.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import re
+import sqlite3
 
 # Regex for finding the beginning of log
 dateRegex = re.compile(r"^[1-9][0-9]{3,} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) +[1-9][0-9]? +[0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.[0-9]{3}")
+numberRegex = re.compile(r"^-?[0-9]+(,[0-9]+)")
 
 # Finds next beginning of the log, skipping everything else
 def findDate(f):
@@ -48,28 +50,30 @@ def slice_columns(header, b, e, prefix):
             yield (prefix.strip() + " " + header[0][last:i].strip()).strip()
 
 # This function parses any table found inside log
-def parseTable(f):
+def parseTable(f, conn, log_id, table_id):
     line = f.next().strip()
     header = []
 
+    # Read header
     while len(line) > 2 and line[1] != '-':
         if line[0] != '|':
             print "Error: not a table"
-            return
+            return False
+
         header.append(line)
         line = f.next().strip()
 
     if len(header) == 0:
         print "Warning: empty header"
     else:
-        print "Header found"
-        for col in slice_columns(header, 1, len(header[0]), ""):
-            print col
+        # Parse header
+        cols = list(slice_columns(header, 1, len(header[0]), ""))
 
     line = f.next().strip()
 
+    # Read table rows
+    row_id = 1
     while len(line) > 0:
-        line = f.next().strip()
 
         row = []
         val = ""
@@ -80,7 +84,18 @@ def parseTable(f):
             else:
                 val += char
 
-        print row
+        # Store row into database
+        for i in range(len(row)):
+            if numberRegex.match(row[i]):
+                row[i] = row[i].replace(",", ".")
+                conn.execute('''INSERT INTO tables_num VALUES (?, ?, ?, ?, ?)''', [log_id, table_id, cols[i], row_id, float(row[i])])
+            else:
+                conn.execute('''INSERT INTO tables_str VALUES (?, ?, ?, ?, ?)''', [log_id, table_id, cols[i], row_id, row[i]])
+
+        line = f.next().strip()
+        row_id += 1
+
+    return True
 
 # Program beginning
 # Open log
@@ -92,6 +107,26 @@ findDate(f)
 # Prepare key-value regex
 kvRegex = re.compile(".*[:=].*")
 
+# By convention IDs in SQL database count from 1, not 0
+log_id = 1
+table_id = 1
+
+# Open database file
+conn = sqlite3.connect("logs.db")
+conn.text_factory = str
+
+# Create tables
+conn.execute('''CREATE TABLE IF NOT EXISTS logs (log_id INT, log_type INT, date TEXT)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS keyvals (log_id INT, key TEXT, value TEXT)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS tables_str (log_id INT, table_id TINYINT, table_col_name TEXT, table_row_id INT, value TEXT)''')
+conn.execute('''CREATE TABLE IF NOT EXISTS tables_num (log_id INT, table_id TINYINT, table_col_name TEXT, table_row_id INT, value DOUBLE)''')
+
+# Remove old content
+conn.execute('''DELETE FROM logs''')
+conn.execute('''DELETE FROM keyvals''')
+conn.execute('''DELETE FROM tables_str''')
+conn.execute('''DELETE FROM tables_num''')
+
 try:
     while True:
         # Iterate over lines
@@ -102,17 +137,29 @@ try:
             pass
 
         # Log beginning found
-        # TODO: store new log information
+        # TODO: store date
         elif dateRegex.match(line):
-            pass
+            # Log type is parsed and stored as number
+            log_type_b = line.find("0x")
+            log_type_e = line[log_type_b:].find(" ")
+            log_type = int(line[log_type_b:log_type_b+log_type_e], 0)
+            conn.execute('''INSERT INTO logs (log_id, log_type) VALUES (?, ?)''', [log_id, log_type] )
+            log_id += 1
+            table_id = 1
+
         # Check if line is beginning of a table
         elif line[0] == '-' or line[0] == '|':
-            parseTable(f)
+            if parseTable(f, conn, log_id, table_id):
+                table_id += 1
 
         # Check if line is key-value
         elif kvRegex.match(line):
             key, value = parseKV(line)
-            print key, value
+            conn.execute('''INSERT INTO keyvals (log_id, key, value) VALUES (?, ?, ?)''', [log_id, key, value])
 
+# Thrown when end of file is reached
 except StopIteration:
     pass
+
+# Confirm changes to database (database is left unchanged in case of program crash)
+conn.commit()
